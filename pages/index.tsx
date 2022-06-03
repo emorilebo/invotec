d26d01e5daa65509b839cd306dd7e1d1ffa6603a
Godfrey Lebo <emorylebo@gmail.com>
2022-06-03 15:48:33 +0100

    UPDATE

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <div className="">
       <Head>
         <title>Invotec Nigeria LTD</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/fzx.png" />
       </Head>
 
       <HomePage />
