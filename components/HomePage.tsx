aaa58ff221ea5a0967cd7af494ce516e3d9ea541
Godfrey Lebo <emorylebo@gmail.com>
2022-06-03 14:43:40 +0100

    UPDATE

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -12,6 +12,12 @@ import {
 } from '@heroicons/react/outline'
 import { ChevronRightIcon, ExternalLinkIcon } from '@heroicons/react/solid'
 
+
+interface Props {
+  
+}
+
+
 const navigation = [
   { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
@@ -295,7 +301,7 @@ const HomePage = () => {
                       href="#"
                       className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
                     >
-                      Start free trial
+                      Goto FZXMEdia Consulting
                     </a>
                   </div>
                   <div className="mt-6 px-5">
@@ -322,27 +328,26 @@ const HomePage = () => {
                       className="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
                     >
                       <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-teal-500 to-cyan-600 rounded-full">
-                        We're hiring
+                        We're connecting
                       </span>
-                      <span className="ml-4 text-sm">Visit our careers page</span>
+                      <span className="ml-4 text-sm">Visit our packages page</span>
                       <ChevronRightIcon className="ml-2 w-5 h-5 text-gray-500" aria-hidden="true" />
                     </a>
                     <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                      <span className="block">A better way to</span>
+                      <span className="block">Welcome to Invotec</span>
                       <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 sm:pb-5">
-                        ship web apps
+                        for innovative tech
                       </span>
                     </h1>
                     <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
-                      Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit
-                      sunt amet fugiat veniam occaecat fugiat.
+                    We're connecting communities, homes, businesses, and individuals through and extremely blazing fast optic fibre connection with unlimited internet connection to satisfy your everyday need.
                     </p>
                     <div className="mt-10 sm:mt-12">
                       <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-0">
                         <div className="sm:flex">
                           <div className="min-w-0 flex-1">
                             <label htmlFor="email" className="sr-only">
-                              Email address
+                              Enter your email
                             </label>
                             <input
                               id="email"
@@ -356,15 +361,14 @@ const HomePage = () => {
                               type="submit"
                               className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
                             >
-                              Start free trial
+                              Interested
                             </button>
                           </div>
                         </div>
                         <p className="mt-3 text-sm text-gray-300 sm:mt-4">
-                          Start your free 14-day trial, no credit card necessary. By providing your email, you agree to
-                          our{' '}
+                          Process is way easier than you think. Doubt it? Drop your email to start the process to enter {' '}
                           <a href="#" className="font-medium text-white">
-                            terms of service
+                            the Internet of Things
                           </a>
                           .
                         </p>
@@ -390,19 +394,18 @@ const HomePage = () => {
           <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
             <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
               <div>
-                <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">Serverless</h2>
+                <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">Connection</h2>
                 <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-                  No server? No problem.
+                  Heavy Data Consumer? No problem.
                 </p>
                 <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-                  Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper
-                  malesuada. Eleifend condimentum id viverra nulla.
+                  We offer unlimited internet connection to your homes, shops, businesses, devices, and all at blazing fast fibre connection with 99.8% uptime to have you covered round the clock..
                 </p>
               </div>
-              <div className="mt-12 -mb-10 sm:-mb-24 lg:-mb-80">
+              <div className="mt-12 ">
                 <img
                   className="rounded-lg shadow-xl ring-1 ring-black ring-opacity-5"
-                  src="https://tailwindui.com/img/component-images/green-project-app-screenshot.jpg"
+                  src="https://miro.medium.com/max/1200/0*ENnYq7BzLL0X6K5t.jpg"
                   alt=""
                 />
               </div>
@@ -451,7 +454,7 @@ const HomePage = () => {
                   <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
                     <img
                       className="object-cover lg:h-full lg:w-full"
-                      src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
+                      src="https://www.unical.edu.ng/assets/img/Prof.jpg"
                       alt=""
                     />
                   </div>
@@ -470,13 +473,12 @@ const HomePage = () => {
                         <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                       </svg>
                       <p className="mt-6 text-2xl font-medium text-white">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue.
-                        Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.
+                        The University of Calabar is entirely connected to the internet with Fibre couresty of Invotec Smart Connect. Making it easy for us to run our day to day activities better and connect with the rest of the world seemlessly and latency free
                       </p>
                     </div>
                     <footer className="mt-6">
-                      <p className="text-base font-medium text-white">Judith Black</p>
-                      <p className="text-base font-medium text-cyan-100">CEO at PureInsights</p>
+                      <p className="text-base font-medium text-white">Prof. Florence Banku Obi</p>
+                      <p className="text-base font-medium text-cyan-100">Vice Chancellor, University of Calabar</p>
                     </footer>
                   </blockquote>
                 </div>
@@ -485,61 +487,7 @@ const HomePage = () => {
           </div>
 
           {/* Blog section */}
-          <div className="relative bg-gray-50 py-16 sm:py-24 lg:py-32">
-            <div className="relative">
-              <div className="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-                <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">Learn</h2>
-                <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-                  Helpful Resources
-                </p>
-                <p className="mt-5 mx-auto max-w-prose text-xl text-gray-500">
-                  Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper
-                  malesuada. Eleifend condimentum id viverra nulla.
-                </p>
-              </div>
-              <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
-                {blogPosts.map((post) => (
-                  <div key={post.id} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-                    <div className="flex-shrink-0">
-                      <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
-                    </div>
-                    <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-cyan-600">
-                          <a href={post.category.href} className="hover:underline">
-                            {post.category.name}
-                          </a>
-                        </p>
-                        <a href={post.href} className="block mt-2">
-                          <p className="text-xl font-semibold text-gray-900">{post.title}</p>
-                          <p className="mt-3 text-base text-gray-500">{post.preview}</p>
-                        </a>
-                      </div>
-                      <div className="mt-6 flex items-center">
-                        <div className="flex-shrink-0">
-                          <a href={post.author.href}>
-                            <img className="h-10 w-10 rounded-full" src={post.author.imageUrl} alt={post.author.name} />
-                          </a>
-                        </div>
-                        <div className="ml-3">
-                          <p className="text-sm font-medium text-gray-900">
-                            <a href={post.author.href} className="hover:underline">
-                              {post.author.name}
-                            </a>
-                          </p>
-                          <div className="flex space-x-1 text-sm text-gray-500">
-                            <time dateTime={post.datetime}>{post.date}</time>
-                            <span aria-hidden="true">&middot;</span>
-                            <span>{post.readingLength} read</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+         
 
           {/* CTA Section */}
           <div className="relative bg-gray-900">
